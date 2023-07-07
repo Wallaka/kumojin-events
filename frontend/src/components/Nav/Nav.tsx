1a295f650d279924f4ff31afa82b870a8738f5d1
Wallaka <antoine.wallaka@gmail.com>
2023-07-07 15:13:54 -0400

    fix(nav): add fixed position

--- frontend/src/components/Nav/Nav.tsx
+++ frontend/src/components/Nav/Nav.tsx
@@ -6,8 +6,8 @@ import Typography from "@mui/material/Typography";
 
 export default function BasicAppBar() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="sticky">
+    <Box sx={{ flexGrow: 1, marginBottom: 8}}>
+      <AppBar position="fixed">
         <Toolbar>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             <NavLink to="/" style={{ textDecoration: "none" }}>
