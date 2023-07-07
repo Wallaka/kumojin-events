c32847125b355e56dd2a52a1c0f6fc94e0c67ca8
Wallaka <antoine.wallaka@gmail.com>
2023-07-07 15:02:24 -0400

    chore(service): replace placeholder array by readable one

--- backend/src/events/service.ts
+++ backend/src/events/service.ts
@@ -4,57 +4,29 @@ import type { Event } from "./types";
 const events = [
   {
     id: 1,
-    name: "ipsum dolor sit amet consectetuer adipiscing elit proin interdum mauris non ligula pellentesque ultrices phasellus id sapien in sapien iaculis congue vivamus metus",
+    name: "K-pop random dance",
     description:
-      "ipsum dolor sit amet consectetuer adipiscing elit proin interdum mauris non ligula pellentesque ultrices phasellus id sapien in sapien iaculis congue vivamus metus",
-    location: "Cabatuan",
+      "This is a random dance event around k-pop popular songs, come and dance without shame, have fun !!",
+    location: "Montreal",
     date: "2023-02-25T14:28:18Z",
-    tz: "Asia/Manila",
-    address: "11874 Dapin Hill",
   },
   {
     id: 2,
-    name: "massa tempor convallis nulla neque libero convallis eget eleifend luctus ultricies eu nibh quisque id justo sit amet sapien dignissim vestibulum vestibulum ante ipsum primis in faucibus orci luctus",
+    name: "Nightwish concert",
     description:
-      "ipsum dolor sit amet consectetuer adipiscing elit proin interdum mauris non ligula pellentesque ultrices phasellus id sapien in sapien iaculis congue vivamus metus",
+      "Come and see nightwish's latest show with their new album 'I love the snow'",
 
-    location: "Haparanda",
+    location: "Helsinki",
     date: "2023-06-05T09:25:38Z",
-    tz: "Europe/Helsinki",
-    address: "65899 Warrior Trail",
   },
   {
     id: 3,
-    name: "faucibus orci luctus et ultrices posuere cubilia curae donec pharetra magna vestibulum aliquet ultrices erat tortor sollicitudin",
+    name: "Pocha MTL",
     description:
-      "ipsum dolor sit amet consectetuer adipiscing elit proin interdum mauris non ligula pellentesque ultrices phasellus id sapien in sapien iaculis congue vivamus metus",
+      "Come and discover and korean culture during our 4 days events at griffintown, pocha mtl",
 
-    location: "Shal’skiy",
+    location: "Montreal",
     date: "2023-01-17T01:29:54Z",
-    tz: "Europe/Moscow",
-    address: "52067 Thompson Center",
-  },
-  {
-    id: 4,
-    name: "habitasse platea dictumst maecenas ut massa quis augue luctus tincidunt nulla mollis",
-    description:
-      "ipsum dolor sit amet consectetuer adipiscing elit proin interdum mauris non ligula pellentesque ultrices phasellus id sapien in sapien iaculis congue vivamus metus",
-
-    location: "Novovorontsovka",
-    date: "2022-03-14T16:29:54Z",
-    tz: "Europe/Zaporozhye",
-    address: "9587 Linden Point",
-  },
-  {
-    id: 5,
-    name: "et ultrices posuere cubilia curae nulla dapibus dolor vel est donec odio justo sollicitudin ut suscipit a feugiat et eros vestibulum ac est lacinia nisi venenatis tristique fusce congue diam",
-    description:
-      "ipsum dolor sit amet consectetuer adipiscing elit proin interdum mauris non ligula pellentesque ultrices phasellus id sapien in sapien iaculis congue vivamus metus",
-
-    location: "Lauro de Freitas",
-    date: "2022-04-10T10:54:50Z",
-    tz: "America/Bahia",
-    address: "04151 Holy Cross Hill",
   },
 ];
 
